a2d19848807f840fe3aec380a42f5099b81bea99
Michael-Andreas <michaelandreas246@gmail.com>
2022-09-15 21:59:25 +0200

    changes hero

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ addEventOnElem(navLinks, "click", closeNavbar);
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
-  if (window.scrollY > 100) {
+  if (window.scrollY > 10) {
     header.classList.add("active");
   } else {
     header.classList.remove("active");
